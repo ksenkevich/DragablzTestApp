85ae7c648997a95b2797d5a866191e6e3b2bf260
Kyle Senkevich <ksenkevich@surroundtech.com>
2015-10-26 15:08:58 -0400

    Initial setup of project

---
+++ DockableTabInterTabClient.cs
@@ -0,0 +1,88 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DragablzTestApp
+{
+    public class DockableTabInterTabClient : IInterTabClient
+    {
+        private TabablzControl _SourceTabablzControl;
+
+        public TabablzControl SourceTabablzControl
+        {
+            get { return _SourceTabablzControl; }
+            set { _SourceTabablzControl = value; }
+        }
+
+        public List<NewTabHost<Window>> NewTabHosts = new List<NewTabHost<Window>>();
+
+        public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            if (SourceTabablzControl == null)
+            {
+                SourceTabablzControl = source;
+                SourceTabablzControl.IsVisibleChanged += SourceTabablzControl_IsVisibleChanged;
+            }
+
+            var window = new DragablzWindow();
+            window.Height = source.ActualHeight;
+            window.Width = source.ActualWidth;
+            window.WindowState = WindowState.Normal;
+            window.SizeChanged += Window_SizeChanged;
+            window.StateChanged += Window_StateChanged;
+
+            var tabControl = new TabablzControl();
+            window.Content = tabControl;
+            var newTabHost = new NewTabHost<Window>(window, tabControl);
+            NewTabHosts.Add(newTabHost);
+            window.Closed += Window_Closed;
+            return newTabHost;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            var window = sender as DragablzWindow;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var window = sender as DragablzWindow;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var tabHost = NewTabHosts.FirstOrDefault(x => x.Container == (Window)sender);
+            if (tabHost != null)
+            {
+                NewTabHosts.Remove(tabHost);
+            }
+        }
+
+        private void SourceTabablzControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var tabControl = (TabablzControl)sender;
+            foreach (var host in NewTabHosts)
+            {
+                var window = host.Container;
+
+                if (tabControl.IsVisible)
+                {
+                    window.Show();
+                }
+                else
+                {
+                    window.Hide();
+                }
+            }
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            return TabEmptiedResponse.CloseWindowOrLayoutBranch;
+        }
+    }
+}
